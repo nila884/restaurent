cb868979ab53a6efe2493b217dca7c3393f198bf
nila <nilaniyera@gmail.com>
2020-03-25 23:17:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const selectElement=function(param) {
+    
+    return document.querySelector(param)
+}
+
+let menuToggle=selectElement('.menu-toggle');
+let body=selectElement('body')
+
+menuToggle.addEventListener('click',function(){
+    body.classList.toggle('open');
+})
+
+//scroll
+
+window.sr= ScrollReveal();
+sr.reveal('.animate-left',{
+    origin:'left',
+    duration:1000,
+    distance: '25rem',
+    delay: 300
+})
+
+window.sr= ScrollReveal();
+sr.reveal('.animate-right',{
+    origin:'right',
+    duration:1000,
+    distance: '25rem',
+    delay: 600
+})
+
+window.sr= ScrollReveal();
+sr.reveal('.animate-top',{
+    origin:'top',
+    duration:1000,
+    distance: '25rem',
+    delay: 300
+})
+window.sr= ScrollReveal();
+sr.reveal('.animate-bottom',{
+    origin:'bottom',
+    duration:1000,
+    distance: '25rem',
+    delay: 300
+})
